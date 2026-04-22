8d06d1405ea0d40c1368a0520d28476b3e780b91
AlexLe08 <8702466+AlexLe08@users.noreply.github.com>
2026-04-22 12:29:43 -0400

    Add table booking API implementation

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,52 +1,62 @@
 import "./main.css";
-import { useReducer } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { useReducer, useEffect } from "react";
+import { Routes, Route } from "react-router";
 import HomePage from "../pages/HomePage/HomePage";
 import BookingPage from "../pages/BookingPage/BookingPage";
-
-const initializeTimes = () => {
-  return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-};
+import { fetchAPI } from "../../fetchAPI";
 
 const updateTimes = (state, action) => {
   switch (action.type) {
     case "update_date":
-      console.log("Updating times for date:", action.payload);
-      return initializeTimes();
+      const date = new Date(action.payload);
+      const times = fetchAPI(date);
+      return times;
     default:
       return state;
   }
 };
 
+const initializeTimes = () => {
+  const date = new Date();
+  const times = fetchAPI(date);
+  return times;
+};
+
 const MainSection = () => {
   const [availableTimes, dispatch] = useReducer(
     updateTimes,
-    undefined,
+    [],
     initializeTimes,
   );
 
+  useEffect(() => {
+    function fetchInitialTimes() {
+      const times = initializeTimes();
+      dispatch({ type: "update_date", payload: times });
+    }
+    fetchInitialTimes();
+  }, []);
+
   return (
     <main>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/booking"
-            element={
-              <BookingPage
-                availableTimesReducer={{
-                  availableTimes: availableTimes,
-                  dispatch: dispatch,
-                }}
-              />
-            }
-          />
-          <Route path="/about" element={<h1>About Page</h1>} />
-          <Route path="/menu" element={<h1>Menu Page</h1>} />
-          <Route path="/order-online" element={<h1>Order Online Page</h1>} />
-          <Route path="/login" element={<h1>Login Page</h1>} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/booking"
+          element={
+            <BookingPage
+              availableTimesReducer={{
+                availableTimes: availableTimes,
+                dispatch: dispatch,
+              }}
+            />
+          }
+        />
+        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/menu" element={<h1>Menu Page</h1>} />
+        <Route path="/order-online" element={<h1>Order Online Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+      </Routes>
     </main>
   );
 };
---
+++ src/components/pages/BookingPage/ConfirmBookingPage.js
@@ -0,0 +1,13 @@
+const BookingConfirmationPage = () => {
+  return (
+    <div className="booking-confirmation-page">
+      <h1>Booking Confirmed!</h1>
+      <p>
+        Thank you for your reservation. We look forward to serving you at Little
+        Lemon!
+      </p>
+    </div>
+  );
+};
+
+export default BookingConfirmationPage;
